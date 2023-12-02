3ce2a2598a29a624278fd2ed8748729db03a2740
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-12-02 14:20:45 -0600

    cleanup

--- aoc2023/second.clj
+++ aoc2023/second.clj
@@ -9,7 +9,7 @@
 
 (defn parse-pairs [sq] 
     (->> sq (map #(str/split % #" "))
-            (map #(vec [(second %) (Integer/parseInt (first %))]))))
+            (map (fn [[k v]] [v (Integer/parseInt k)]))))
 
 (defn str->map [v]
      (->> v (map #(re-seq #"\d+ \w+" %))
@@ -41,31 +41,12 @@
          (map-indexed determine-possible)
          (apply +)))
 
-
 (defn solution-2 [arr]
     (->> arr 
-         (map (fn [sq] (apply merge-with max sq)))
-         (map vals)
-         (map #(apply * %))
+         (map #(apply merge-with max %))
+         (map #(apply * (vals %)))
          (apply +)))
 
-;(map #(apply merge-with + %) (read)) 
-#_(defn solution-1 [arr] 
-  (let [threshold { "red" 12 "green" 13 "blue" 14 }
-        summed-scores (map #(apply merge-with + %) arr)] 
-        (->> summed-scores 
-             (map-indexed #(if (cmp-map threshold %2) 
-                             (inc %1)
-                             0))
-             (reduce +)
-             )))
-(def test 
-"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
- Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
- Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
- Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
- Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
-#_(println (solution-1 (parse (str/split-lines test))))
 
 (println (solution-1 (parse (input "./input2.txt"))))
 
