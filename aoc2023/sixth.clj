eddcef49272b95a820b6bd956e67a5cf71bdd259
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-12-06 11:07:10 -0600

    refactor 6

--- aoc2023/sixth.clj
+++ aoc2023/sixth.clj
@@ -6,28 +6,16 @@
   "Time:      7  15   30
    Distance:  9  40  200")
 
-
 (defn after-colon [st] 
   (inc (str/index-of st ":" 4)))
-;Your toy boat has a starting speed of zero millimeters per millisecond. 
-;For each whole millisecond you spend at the beginning of the race holding down the button,
-;the boat's speed increases by one millimeter per millisecond.
-; holding button for x 
-; goes for y
-; hold for 0, goes for 0
-; holds for 1, goes for 
-
-
-(defn rem-time [time hold] 
-  (- time hold))
 
 (defn distance [hold time]
-  (* hold (rem-time time hold)))
+  (* hold (- time hold)))
 
-(defn get-data [url, transform-str] 
+(defn get-data [url transform-str] 
   ( ->> (u/input url) 
         (map #(subs % (after-colon %)))
-        (map #(read-string (transform-str %)))))
+        (map #(-> % transform-str read-string))))
 
 (defn transform1 [s] 
   (str "[" s "]"))
@@ -48,17 +36,16 @@
  (let [distances (possible-distances t)]
   (conj acc (keep-threshold distances d))))
 
-(defn solution1 [input] 
-  (let [matrix (transpose (get-data input transform1))]
+(defn solution [input transform] 
+  (let [matrix (transpose (get-data input transform))
+        _ (println matrix) ]
       (apply * (map count (reduce calc-distances [] matrix)))))
 
-(defn solution2 [input] 
-   (let [matrix (transpose (get-data input transform2))]
-      (apply * (map count (reduce calc-distances [] matrix)))))
 (defn -main [ ]
-  (let [s1 (solution1 "./aoc2023/input6.txt")
-        s2 (solution2 "./aoc2023/input6.txt")] 
+  (let [s1 (solution "./aoc2023/input6.txt" transform1)
+        s1 (solution test-input transform1)
+        #__s2 #_(solution2 "./aoc2023/input6.txt")] 
    (do (println s1)
-       (println s2))))
+       #_(println s2))))
 
 
