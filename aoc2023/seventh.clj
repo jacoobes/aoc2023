29495825f28e071b8bb63eb515ff2785412e48fd
jacob <jacoobes@sern.dev>
2023-12-07 16:45:13 -0600

    ss

--- aoc2023/seventh.clj
+++ aoc2023/seventh.clj
@@ -3,10 +3,16 @@
 
 (def test "32T3K 765")
 
+
+
 (defn into-pair [s] 
   (let [[hand wager] (str/split s #" ")]  
     [(frequencies hand) (Integer/parseInt wager)]))
 
+(defn solution [lines]
+  (for [line lines] 
+   ( ->> line 
+         (map into-pair))))
 
 (defn -main [ ]
 
