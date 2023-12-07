6e33f17ac278b2caaa650e07dd02eff252b82338
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-12-07 00:46:45 -0600

    sss

---
+++ aoc2023/seventh.clj
@@ -0,0 +1,15 @@
+(ns seventh
+  (:require [clojure.string :as str]
+            [utils :as u]))
+
+(def test "32T3K 765")
+
+(defn into-pair [s ] 
+  ( ->> (str/split s #" "))
+
+
+(defn -main [ ]
+
+  )
+
+
