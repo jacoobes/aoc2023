ede5082119d250b5263c250e66ed200223a167a5
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-12-12 11:15:32 -0600

    yarg

--- aoc2023/twelve.clj
+++ aoc2023/twelve.clj
@@ -1,5 +1,6 @@
 (:require '[clojure.string :as str])
 (:require '[clojure.java.io :as io])
+(:require [clojure.math.combinatorics :as combo])
 (require '[utils :as u])
 
 (def test 
@@ -12,10 +13,17 @@
 
 (def into-vec (comp read-string #(str "[" % "]" ) second))
 
-(def non-period (comp #_#(re-seq #"[\?|\#]+" %) first))
+(def non-period (comp #(str/split % #"") first))
+
+(defn factorial
+  ([n]                    ; when only one argument is passed in
+    (factorial n 1))
+  ([n acc]                ; when two arguments are passed in
+    (if  (= n 0)  acc
+    (recur (dec n) (* acc n)))))
 
 (->> (u/input test)
      (map #(str/split % #" "))
      (map (juxt non-period into-vec))
-     (map (fn [[f l]] { :pattern f :broken l }))
-     )
+     (map (fn [[f l]] { :pattern f :broken l :max-arrangement (factorial (count f))   })))
+
